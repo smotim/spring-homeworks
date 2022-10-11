99b9a995363b0ed6ca8e89d883d603f261894766
Timofey Smolin <57951811+smotim@users.noreply.github.com>
2022-10-11 14:08:58 +0500

    Initial commit

---
+++ mod1_lesson6/src/main/java/org/example/web/dto/Book.java
@@ -0,0 +1,50 @@
+package org.example.web.dto;
+
+public class Book {
+    private Long id;
+    private String author;
+    private String title;
+    private Integer size;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", author='" + author + '\'' +
+                ", title='" + title + '\'' +
+                ", size=" + size +
+                '}';
+    }
+}
