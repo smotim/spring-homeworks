9eeca04a352c30ed44bfbfd8a61fe91dfc8be772
Timofey Smolin <57951811+smotim@users.noreply.github.com>
2022-11-29 22:08:22 +0500

    bun4

---
+++ buns/bun4/src/main/java/com/zaurtregulov/spring/mvc/MyController.java
@@ -0,0 +1,41 @@
+package com.zaurtregulov.spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+@Controller
+public class MyController {
+
+    @RequestMapping("/")
+    public String showFirstView(){
+        return "first-view";
+    }
+
+    @RequestMapping("/askDetails")
+    public String getEmpDetails(Model model){
+
+        model.addAttribute("employee", new Employee());
+
+        return "ask-emp-details-view";
+    }
+
+    @RequestMapping("/showDetails")
+    public String showEmpDetails(@Valid @ModelAttribute("employee") Employee emp,
+                                 BindingResult bindingResult){
+
+        if(bindingResult.hasErrors()){
+            return "ask-emp-details-view";
+        }
+        else {
+            return "show-emp-details-view";
+        }
+    }
+
+}
---
+++ buns/bun4/src/main/webapp/WEB-INF/view/first-view.jsp
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+<h2>I wish luck in Spring MVC learning</h2>
+<br>
+<br>
+
+<a href="askDetails">Please write your Details</a>
+
+</body>
+
+</html>
